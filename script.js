afcb5060ff9a7e9d2320ddb3ee05b71ff6e67729
kangj064 <113397395+kangj064@users.noreply.github.com>
2023-11-07 09:49:59 -0500

    e

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-var FRAMES = 148;
-var FPS = 30;
-var video = document.getElementById("video");
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
 
-window.addEventListener("scroll", function (e) {
-  var time = ((window.scrollY / 1000) * FRAMES) / FPS;
-  video.currentTime = time;
-  console.log(time);
-  // alert('Hizo scroll')
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 9000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+//Video Animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", (e) => {
+  scrollpos = e.scrollPos / 1000;
 });
 
-window.addEventListener("load", function (e) {
-  video.pause();
-  video.currentTime = 0;
-});
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  console.log(scrollpos, delay);
+
+  video.currentTime = delay;
+}, 33.3);
